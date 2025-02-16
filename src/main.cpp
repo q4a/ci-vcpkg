0c923b46e043a7e84fa9f868e342d2b028e2487c
Aleksey Komarov <q4arus@ya.ru>
2025-02-16 16:27:53 +0300

    vcpkg: add directxmath. Also add simple function null_check

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,14 @@
 
 #include <cstdio>
 
+inline bool null_check(const char* Str0)
+{
+  if (Str0 == NULL)
+    return true;
+  else
+    return false;
+}
+
 // main function!
 int main() {
   fmt::print("Hello, world!\n");
