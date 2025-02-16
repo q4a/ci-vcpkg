924fb188f225e073fcc8166d57495023e25cef77
Aleksey Komarov <q4arus@ya.ru>
2025-02-16 16:10:21 +0300

    vcpkg: remove boost-config and sqlite3. Also enable clang-tidy

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,10 @@
 // main.cpp
 #include <fmt/core.h>
-#include <sqlite3.h>
 
 #include <cstdio>
 
 // main function!
 int main() {
   fmt::print("Hello, world!\n");
-  fmt::print("{}\n", sqlite3_libversion());
   return 0;
 }
